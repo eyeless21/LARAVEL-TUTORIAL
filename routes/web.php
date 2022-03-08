008a908119eb877821581f43d64534d46ed3cf16
eyeless21 <apostol.aras619@hotmail.com>
2022-03-08 17:42:36 +0200

    more changes

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,23 @@ use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\PostCommentsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('ping',function(){
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+$mailchimp->setConfig([
+	'apiKey' => config('services.mailchimp.key'),
+	'server' => 'us6'
+]);
+
+$response = $mailchimp->lists->addListMember('d3c0c95629',[
+    'email_address' => 'jeffjordanway@gmail.com',
+    'status' => 'subscribed'
+]);
+
+//$response = $mailchimp->ping->get();
+// print_r($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);

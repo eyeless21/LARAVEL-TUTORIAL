de73942aa3f1fd94fa9e3ccd1f9b4ff3642cff97
eyeless21 <apostol.aras619@hotmail.com>
2022-03-10 13:17:21 +0200

    section 67 commit

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,13 +1,12 @@
 @props(['name'])
 <x-form.field>
-                <x-form.label name=" {{ $name }}" />
+        <x-form.label name=" {{ $name }}" />
 
-                    <input class="border border-gray-400 p-2 w-full"
-                           type= "{{ $type }}"
-                           name="{{ $name }}"
-                           id="{{ $name }}"
-                           value="{{ old($name) }}"
-                           required>
+            <input class="border border-gray-400 p-2 w-full"
+                name="{{ $name }}"
+                id="{{ $name }}"
+                {{ $attributes(['value' => old($name)])}}
+                >
 
-                           <x-form.error name=" {{  $name }}" />
+        <x-form.error name=" {{  $name }}" />
 </x-form.field>
